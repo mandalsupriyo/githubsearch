38b61cbc2dc71d16dc32f39030ce8c1cfe858b67
Mandal <S.Mandal@zbw-nett.zbw-kiel.de>
2022-07-05 13:35:56 +0200

    Updates

---
+++ precache-manifest.bac3081cd3cc6cdfb434776c36b09176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eabca5e394cc98f821814682687e00c",
+    "url": "/githubsearch/index.html"
+  },
+  {
+    "revision": "eac2caaf1a267319b92b",
+    "url": "/githubsearch/static/css/main.ae8ec85a.chunk.css"
+  },
+  {
+    "revision": "508b5e7ada8e86353076",
+    "url": "/githubsearch/static/js/2.7a0f2d9d.chunk.js"
+  },
+  {
+    "revision": "eac2caaf1a267319b92b",
+    "url": "/githubsearch/static/js/main.469d3a70.chunk.js"
+  },
+  {
+    "revision": "d9080d0a4293e45b14dd",
+    "url": "/githubsearch/static/js/runtime~main.5c46d1be.js"
+  },
+  {
+    "revision": "21cf2782fd717ab683f459e7342e3bcc",
+    "url": "/githubsearch/static/media/spinner.21cf2782.gif"
+  }
+]);
